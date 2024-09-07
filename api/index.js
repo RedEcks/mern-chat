59340e1fa17d92f0ce7d80c1b4a17a944f6ebd3a
Xavier Dunkley <xavierxv18@gmail.com>
2024-09-07 14:22:13 -0400

    added connections to mongodb

--- api/index.js
+++ api/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const jwt = require('jsonwebtoken')
+const User = require('./models/user')
 
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL)
+
+console.log(process.env.MONGO_URL)
 const app =  express();
 
 app.get('/test', (req,res) =>{
     res.json('its alive')
 });
 
+app.post('/register',async(req,res)=>{
+    const {username,password} = req.body;
+    const createdUser = await User.create({username,password})
+    jwt.sign({userId:createdUser._id}, )
+})
+
 
 app.listen(4000);
+
+
