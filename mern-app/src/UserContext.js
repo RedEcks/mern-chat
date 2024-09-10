d378c207b0f120b5f282e798da7575610f030dbb
Xavier Dunkley <xavierxv18@gmail.com>
2024-09-09 22:12:52 -0400

    added cookie parser

--- mern-app/src/UserContext.js
+++ mern-app/src/UserContext.js
@@ -1,10 +1,17 @@
-import { createContext, useState } from 'react';
+import axios from 'axios';
+import { createContext, useEffect, useState } from 'react';
 
 export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
     const [username, setLoggedInUsername] = useState(null);
     const [id, setId] = useState(null);
+
+    useEffect(()=>{
+        axios.get('/profile', {withCredentials:true}).then(response =>{
+            console.log(response.data)
+        })
+    },[])
 
     return (
         <UserContext.Provider value={{ username, setLoggedInUsername, id, setId }}>
