6faba487d5f74edcf447dd979b0aa2b7b2d87a39
Xavier Dunkley <xavierxv18@gmail.com>
2024-09-23 02:02:31 -0400

    added websockets to chat component

--- mern-app/src/Chat.js
+++ mern-app/src/Chat.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Chat() {
+  useEffect(() => {
+    new WebSocket('ws://localhost:4000');
+  },[])
   return (
     <div className='flex h-screen'>
       <div className='bg-white w-1/3'>
